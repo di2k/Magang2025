66dec6714c2b9c0e76643ed9590debd6999afbc6
Didik Setiawan <didik.s@kemenkeu.go.id>
2025-03-04 14:05:10 +0700

    Update

---
+++ application/views/telaah/index.php
@@ -0,0 +1,18 @@
+<div class="page-wrapper">
+    <div class="container-fluid">
+        <div class="page-header d-print-none" style="margin-top: 10px;">
+            <div class="row align-items-center">
+
+                <div class="col">
+                    <div class="page-pretitle">
+                        RAG
+                    </div>
+                    <h2 class="page-title">
+                        SAPA Anggaran
+                    </h2>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
